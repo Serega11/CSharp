01b6d5c2e0db30e789b1113aa5314ee9e8c052f1
Serega S <dial16046971@ya.ru>
2015-11-01 03:01:38 +0300

    begin

---
+++ LessonDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LessonDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringHelper helper = new StringHelper();
+
+            CountDelegate d1 = helper.GetCount;
+            CountDelegate d2 = helper.GetCountSymbolA;
+
+            //CountDelegate error = helper.GetCountSymbol; //Нельзя использовать, т.к. дилегат имеет отличную сигнатуру int(string,char)
+
+            string testString = "LAMP";
+
+            Console.WriteLine("Общее количество симаолов: {0}",TestDelegate(d1,testString));
+            Console.WriteLine("Количество символов А: {0}", TestDelegate(d2, testString));
+
+            Console.ReadLine();
+        }
+        //Объявим метод (ссылка на тест-м метод, тестируемая строка)
+        static int TestDelegate(CountDelegate method, string testString)
+        {
+            return method(testString);
+        }
+    }
+}
---
+++ LessonDelegate/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LessonDelegate
+{
+    //class Class1
+    //{
+    //}
+    public class Student
+    {
+        public string Move(int distance)
+        {
+            return null;
+        }
+
+        public int GetAge()
+        {
+            return 17;
+        }
+
+        public string Grow(int years)
+        {
+            return null;
+        }
+
+        public void SetInfo(string name, int age)
+        { }
+
+        public void SetSchool(int id, string number )
+        { }
+    }
+}
